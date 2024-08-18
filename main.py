bc70e3b93ade8dd80148e93ecf21ce6207382883
saitejakavuru <workbox.teja@gmail.com]>
2024-08-18 14:18:45 +0100

    no issues till here so commiting

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.smartfarm import logger
+from src.smartfarm.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
