4d9b69a554480f3a616b2f96ece2cd941373126c
saitejakavuru <workbox.teja@gmail.com]>
2024-08-22 14:57:56 +0100

    data transform

--- src/smartfarm/config/configuration.py
+++ src/smartfarm/config/configuration.py
@@ -1,7 +1,8 @@
 from smartfarm.constants import *
 from smartfarm.utils.common import read_yaml, create_directories
 from smartfarm.entity.config_entity import (DataIngestionConfig,
-                                            DataValidationConfig)
+                                            DataValidationConfig,
+                                            DataTransformationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -32,6 +33,8 @@ class ConfigurationManager:
 
         return data_ingestion_config
     
+    
+    
     def get_data_validation_config(self) -> DataValidationConfig:
         config = self.config.data_validation
         schema = self.schema.COLUMNS
@@ -46,3 +49,17 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
