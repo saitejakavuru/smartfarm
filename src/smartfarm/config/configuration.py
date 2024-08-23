6821163d7d504fa63773a908fb688e078fe61877
saitejakavuru <workbox.teja@gmail.com]>
2024-08-23 12:38:43 +0100

    model trainer finished

--- src/smartfarm/config/configuration.py
+++ src/smartfarm/config/configuration.py
@@ -2,7 +2,8 @@ from smartfarm.constants import *
 from smartfarm.utils.common import read_yaml, create_directories
 from smartfarm.entity.config_entity import (DataIngestionConfig,
                                             DataValidationConfig,
-                                            DataTransformationConfig)
+                                            DataTransformationConfig,
+                                            ModelTrainerConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -63,3 +64,25 @@ class ConfigurationManager:
         )
 
         return data_transformation_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.RandomForest  # Use 'RandomForest' instead of 'ElasticNet'
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            train_data_path=config.train_data_path,
+            test_data_path=config.test_data_path,
+            model_name=config.model_name,
+            n_estimators=params.n_estimators,
+            max_depth=params.max_depth,
+            min_samples_split=params.min_samples_split,
+            min_samples_leaf=params.min_samples_leaf,
+            max_features=params.max_features,
+            bootstrap=params.bootstrap,
+            target_column=schema.name
+        )
+        return model_trainer_config
--- src/smartfarm/entity/config_entity.py
+++ src/smartfarm/entity/config_entity.py
@@ -21,3 +21,17 @@ class DataValidationConfig:
 class DataTransformationConfig:
     root_dir: Path
     data_path: Path
+    
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    train_data_path: Path
+    test_data_path: Path
+    model_name: str
+    n_estimators: int
+    max_depth: int
+    min_samples_split: int
+    min_samples_leaf: int
+    max_features: str
+    bootstrap: bool
+    target_column: str
