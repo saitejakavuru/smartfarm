de14f94030af52219a1de4cfd11d2f4436d0fe6c
saitejakavuru <workbox.teja@gmail.com]>
2024-08-25 17:58:14 +0100

    commiting before deployment

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask,render_template, request
+import os
+import numpy as np
+from smartfarm.pipeline.prediction import PredictionPipeline
+
+app = Flask(__name__)  # initializing a flask app
+
+@app.route('/', methods=['GET'])  # route to display the home page
+def homePage():
+    return render_template("index.html")
+
+@app.route('/train', methods=['GET'])  # route to train the pipeline
+def training():
+    os.system("python main.py")
+    return "Training Successful!" 
+
+@app.route('/predict', methods=['POST', 'GET'])  # route to show the predictions in a web UI
+def index():
+    if request.method == 'POST':
+        try:
+            # Reading the inputs given by the user
+            nitrogen = float(request.form['nitrogen'])
+            phosphorus = float(request.form['phosphorus'])
+            potassium = float(request.form['potassium'])
+            temperature = float(request.form['temperature'])
+            humidity = float(request.form['humidity'])
+            ph = float(request.form['ph'])
+            rainfall = float(request.form['rainfall'])
+
+            data = [nitrogen, phosphorus, potassium, temperature, humidity, ph, rainfall]
+            data = np.array(data).reshape(1, 7)
+            
+            # Prediction pipeline object
+            obj = PredictionPipeline()
+            predict = obj.predict(data)
+
+            # Render results in the results.html template
+            return render_template('results.html', prediction=str(predict))
+
+        except Exception as e:
+            print('The Exception message is: ', e)
+            return 'Something went wrong, please try again.'
+
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
