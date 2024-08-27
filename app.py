15d70df70eed3d78e80dfe5e746c83c89c27c1af
saitejakavuru <workbox.teja@gmail.com]>
2024-08-27 01:33:46 +0100

    rerun

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-from flask import Flask,render_template, request
-import os
+from flask import Flask, render_template, request
+import os 
 import numpy as np
+import pandas as pd
 from smartfarm.pipeline.prediction import PredictionPipeline
 
-app = Flask(__name__)  # initializing a flask app
 
-@app.route('/', methods=['GET'])  # route to display the home page
+app = Flask(__name__) # initializing a flask app
+
+@app.route('/',methods=['GET'])  # route to display the home page
 def homePage():
     return render_template("index.html")
 
-@app.route('/train', methods=['GET'])  # route to train the pipeline
+
+@app.route('/train',methods=['GET'])  # route to train the pipeline
 def training():
     os.system("python main.py")
     return "Training Successful!" 
 
-@app.route('/predict', methods=['POST', 'GET'])  # route to show the predictions in a web UI
+
+@app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
 def index():
     if request.method == 'POST':
         try:
@@ -30,19 +34,19 @@ def index():
             data = [nitrogen, phosphorus, potassium, temperature, humidity, ph, rainfall]
             data = np.array(data).reshape(1, 7)
             
-            # Prediction pipeline object
             obj = PredictionPipeline()
             predict = obj.predict(data)
 
-            # Render results in the results.html template
-            return render_template('results.html', prediction=str(predict))
+            return render_template('results.html', prediction = str(predict))
 
         except Exception as e:
-            print('The Exception message is: ', e)
-            return 'Something went wrong, please try again.'
+            print('The Exception message is: ',e)
+            return 'something is wrong'
 
     else:
         return render_template('index.html')
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+	# app.run(host="0.0.0.0", port = 8080, debug=True)
+	app.run(host="0.0.0.0", port = 8080)
